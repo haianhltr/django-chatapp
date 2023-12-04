fde1a5d39fd457b12cf53f3ab8670c9db551adfd
Anh Le <haianhltr@gmail.com>
2023-12-03 18:02:26 -0600

    set up the app

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_wq%cf$ujsfk=_pfq$8e6g=j#9xia254sg6o^*#7a3wi-!g9c'
+SECRET_KEY = 'django-insecure-8ev-(via#_so7jz!==ccqsyq&@&r0m0#-$mfgvrd=nh$lw5cf1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatapp',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
-
+ASGI_APPLICATION = 'app.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
